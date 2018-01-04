1bc0b788f4eaff0c6d178cb3af6d10e905a3a942
Alberto Gualis <agualis@gmail.com>
2018-01-04 09:50:18 +0100

    First commit with jest setup

---
+++ src/_tests_/TripService.spec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let TripService = require('../TripService');
+
+describe('TripService', () => {
+
+    it('throws error when used not logged in', () => {
+        const tripService = new TripService()
+        expect(() => tripService.getTripsByUser(null))
+          .toThrowError("User not logged in.")
+    });
+
+    xit('get user friends trip list', () => {
+        const LOGGED_USER = 'any user'
+        const TRIP_DAO = {}
+        const TRIP_LIST = ['any trip']
+        const user = {}
+        user.getFriends = jest.fn(() => [LOGGED_USER])
+        TRIP_DAO.findTripsByUser = jest.fn(() => TRIP_LIST)
+        const tripService = new TripService(LOGGED_USER, TRIP_DAO)
+        expect(tripService.getTripsByUser(user)).toBe(TRIP_LIST)
+    });
+});
