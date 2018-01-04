9c7068476a7689d0f828b3295f7fe53eead55186
Alberto Gualis <agualis@gmail.com>
2018-01-04 10:16:01 +0100

    Test next shortest branch: inside loop with but no friends

--- src/_tests_/TripService.spec.js
+++ src/_tests_/TripService.spec.js
@@ -18,4 +18,20 @@ describe('TripService', () => {
         const user = { getFriends: () => [] }
         expect(tripService.getTripsByUser(user)).toEqual([])
     });
+
+    it('returns an empty list when user and loggedUser are no friends ', () => {
+        const LOGGED_USER = 'any user'
+        const tripService = new TripService()
+        tripService.currentUser = jest.fn(()=> LOGGED_USER)
+        const user = { getFriends: () => ['firend1', 'friend2'] }
+        expect(tripService.getTripsByUser(user)).toEqual([])
+    });
+
+    it('returns user trips when user and loggedUser are friends ', () => {
+        const LOGGED_USER = 'any user'
+        const tripService = new TripService()
+        tripService.currentUser = jest.fn(()=> LOGGED_USER)
+        const user = { getFriends: () => [LOGGED_USER] }
+        expect(tripService.getTripsByUser(user)).toEqual([])
+    });
 });
